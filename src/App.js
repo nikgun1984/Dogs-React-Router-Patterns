5760df2c1d2701319fc1c8914c73d31ebd1b1eaf
Nick Gundobin <ngundobin@gmail.com>
2021-02-22 15:10:09 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route,Switch} from 'react-router-dom';
+import DogList from './DogList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div className="App">
+			<header className="App-header">Dogs</header>
+			<main>
+				<Switch>
+					<Route exact path="/dogs">
+						<DogList />
+					</Route>
+				</Switch>
+			</main>
+		</div>
+	);
 }
 
 export default App;
